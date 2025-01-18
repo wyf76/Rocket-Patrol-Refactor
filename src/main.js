bd3fe3ea938c8f25e3633807b85dd2c795e4b39f
wyf76 <yufanweng76@gmail.com>
2025-01-17 21:40:10 -0800

    yes

--- src/main.js
+++ src/main.js
@@ -10,3 +10,4 @@ let game = new Phaser.Game(config)
 //set UI sizes
 let borderUISize = game.config.height/15
 let borderPadding = borderUISize /3
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,12 +8,71 @@ class Menu extends Phaser.Scene{
         this.load.image('rocket', '/assets/rocket.png')
         this.load.image('spaceship', '/assets/spaceship.png')
         this.load.image('starfield', '/assets/starfield.png')
+        this.load.spritesheet('explosion', '/assets/explosion.png',{
+            frameWidth: 64,
+            frameHeight: 32,
+            startFrame: 0,
+            endFrame: 9
+        })
+        // load audio
+        this.load.audio('sfx-select', '/assets/sfx-select.wav')
+        this.load.audio('sfx-explosion', '/assets/sfx-explosion.wav')
+        this.load.audio('sfx-shot', '/assets/sfx-shot.wav')
     }
       
 
     create(){
-        this.add.text(20,20,"Rocket Patrol Menu")
-        this.scene.start("playScene")
+        this.anims.create({
+            key: 'explode',
+            frames: this.anims.generateFrameNumbers('explosion', {start: 0, end: 9, first: 0}),
+            frameRate: 30
+        })
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+              top: 5,
+              bottom: 5,
+            },
+            fixedWidth: 0
+        }
+    // display menu text
+    this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL',
+        menuConfig).setOrigin(0.5)
+    this.add.text(game.config.width/2, game.config.height/2, 'Use arrows to move & (F) to fire',
+        menuConfig).setOrigin(0.5)
+    menuConfig.backgroundColor = '#00FF00'
+    menuConfig.color = '#000'
+    this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press <- for novice or -> for Expert', 
+        menuConfig).setOrigin(0.5)
+
+    // define keys
+    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
     }
+    
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+          // easy mode
+          game.settings = {
+            spaceshipSpeed: 3,
+            gameTimer: 60000    
+          }
+          this.sound.play('sfx-select')
+          this.scene.start('playScene')    
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+          // hard mode
+          game.settings = {
+            spaceshipSpeed: 4,
+            gameTimer: 45000    
+          }
+          this.sound.play('sfx-select')
+          this.scene.start('playScene')    
+        }
+      }
 }
 
