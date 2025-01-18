bbe017b282c712dee61309efac5f289ee7841128
wyf76 <yufanweng76@gmail.com>
2025-01-17 22:06:01 -0800

    yes

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,19 +5,19 @@ class Menu extends Phaser.Scene{
 
     preload() {
         // load images/tile sprites
-        this.load.image('rocket', './assets/rocket.png')
-        this.load.image('spaceship', './assets/spaceship.png')
-        this.load.image('starfield', './assets/starfield.png')
-        this.load.spritesheet('explosion', './assets/explosion.png',{
+        this.load.image('rocket', '/assets/rocket.png')
+        this.load.image('spaceship', '/assets/spaceship.png')
+        this.load.image('starfield', '/assets/starfield.png')
+        this.load.spritesheet('explosion', '/assets/explosion.png',{
             frameWidth: 64,
             frameHeight: 32,
             startFrame: 0,
             endFrame: 9
         })
         // load audio
-        this.load.audio('sfx-select', './assets/sfx-select.wav')
-        this.load.audio('sfx-explosion', './assets/sfx-explosion.wav')
-        this.load.audio('sfx-shot', './assets/sfx-shot.wav')
+        this.load.audio('sfx-select', '/assets/sfx-select.wav')
+        this.load.audio('sfx-explosion', '/assets/sfx-explosion.wav')
+        this.load.audio('sfx-shot', '/assets/sfx-shot.wav')
     }
       
 
